2d4780b8667db95793b4d636ccd6a15ae8f8334c
parroty <parroty00@gmail.com>
2016-01-31 17:25:03 +0900

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule ExTwitterProxy.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :extwitter]]
   end
 
   # Dependencies can be Hex packages:
